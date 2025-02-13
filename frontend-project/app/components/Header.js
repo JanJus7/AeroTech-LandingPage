6c8895ab011c6b500b236cdfc00fdca158095f54
Jan <janjus2004@gmail.com>
2025-02-13 15:44:59 +0100

    added useScrollPosition hook to identify the scroll position and addust the header

--- frontend-project/app/components/Header.js
+++ frontend-project/app/components/Header.js
@@ -2,42 +2,51 @@
 
 import { FaPaperPlane, FaBars, FaTimes } from "react-icons/fa";
 import { useState } from "react";
+import { useScrollPosition } from "../hooks/useScrollPosition";
 
 export default function Header() {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const scrollY = useScrollPosition();
 
   const toggleMenu = () => {
     setIsMenuOpen(!isMenuOpen);
   };
 
+  const isPastHero = scrollY > window.innerHeight;
+
   return (
-    <header className="bg-white shadow-md fixed top-0 left-0 right-0 z-50">
+    <header
+      className={`fixed top-0 left-0 w-full p-4 transition-colors duration-300 z-50 ${
+        isPastHero ? "bg-white text-gray-600" : "bg-gradient-to-r from-blue-800 via-purple-800 to-indigo-900 text-white"
+      }`}
+    >
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 flex justify-between items-center h-16">
-        <div className="flex items-center gap-2 text-xl font-bold text-blue-600">
+        <div className={`flex items-center gap-2 text-xl font-bold ${
+          isPastHero ? "text-blue-600" : "text-white"
+        }`}
+        >
           <FaPaperPlane className="size-6" />
           AeroTech
         </div>
 
         <nav className="hidden md:flex space-x-8">
-          <a href="#about" className="text-gray-600 hover:text-blue-600">
+          <a href="#about" className="hover:text-blue-600">
             About Us
           </a>
-          <a href="#services" className="text-gray-600 hover:text-blue-600">
+          <a href="#services" className="hover:text-blue-600">
             Services
           </a>
-          <a
-            href="#most-tracked-flights"
-            className="text-gray-600 hover:text-blue-600"
-          >
+          <a href="#most-tracked-flights" className="hover:text-blue-600">
             Most Tracked Flights
           </a>
-          <a href="#testimonials" className="text-gray-600 hover:text-blue-600">
+          <a href="#testimonials" className="hover:text-blue-600">
             Testimonials
           </a>
-          <a href="#case-studies" className="text-gray-600 hover:text-blue-600">
+          <a href="#case-studies" className="hover:text-blue-600">
             Case Studies
           </a>
-          <a href="#contact" className="text-gray-600 hover:text-blue-600">
+          <a href="#contact" className="hover:text-blue-600">
             Contact
           </a>
         </nav>
@@ -48,16 +57,18 @@ export default function Header() {
             className="text-gray-600 hover:text-blue-600 focus:outline-none"
           >
             {isMenuOpen ? (
-              <FaTimes className="w-6 h-6" />
+              <FaTimes className="text-white w-6 h-6" />
             ) : (
-              <FaBars className="w-6 h-6" />
+              <FaBars className="text-white w-6 h-6" />
             )}
           </button>
         </div>
       </div>
 
       {isMenuOpen && (
-        <nav className="md:hidden bg-white shadow-md">
+        <nav className="bg-gradient-to-r from-blue-500 via-purple-500 to-indigo-600 md:hidden shadow-md">
+          <div className="absolute inset-0 bg-black opacity-50"></div>
+
           <a
             href="#about"
             className="block px-4 py-2 text-gray-600 hover:text-blue-600"
